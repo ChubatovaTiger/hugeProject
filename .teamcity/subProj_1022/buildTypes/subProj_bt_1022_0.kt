71aa7a46b43881bd737afdb2d7f63ab656603d5f
admin <admin@TeamCity>
2021-12-02 11:41:20 +0100

    TeamCity change in 'bigproject / subProj 1022' project: parameters of 'bt 1022 0' build configuration were updated

--- .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
+++ .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
@@ -6,6 +6,6 @@ object subProj_bt_1022_0 : BuildType({
     name = "bt 1022 0"
 
     params {
-        password("ppwd", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9cd")
+        password("env.ppwd", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9cd")
     }
 })
