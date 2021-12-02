82a59484a70cb79ca3e09f274903d684d8c3ed0c
admin <admin@TeamCity>
2021-12-02 11:40:49 +0100

    TeamCity change in 'bigproject / subProj 1022' project: parameters of 'bt 1022 0' build configuration were updated

--- .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
+++ .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
@@ -4,4 +4,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
 object subProj_bt_1022_0 : BuildType({
     name = "bt 1022 0"
+
+    params {
+        password("ppwd", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9cd")
+    }
 })
