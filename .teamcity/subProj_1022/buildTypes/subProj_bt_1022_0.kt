dbd33e3085a0f8dd5ba456fcab6f59433b029d04
admin <admin@TeamCity>
2021-12-02 11:41:37 +0100

    TeamCity change in 'bigproject / subProj 1022' project: runners of 'bt 1022 0' build configuration were updated

--- .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
+++ .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
@@ -1,11 +1,18 @@
 package subProj_1022.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object subProj_bt_1022_0 : BuildType({
     name = "bt 1022 0"
 
     params {
         password("env.ppwd", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9cd")
+    }
+
+    steps {
+        script {
+            scriptContent = "echo %env.ppwd% > a.txt"
+        }
     }
 })
