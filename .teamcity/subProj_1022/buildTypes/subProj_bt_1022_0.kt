87c515767aa8931d4d968b3731b4ddeaa00a9f4e
admin <admin@TeamCity>
2021-12-02 11:42:12 +0100

    TeamCity change in 'bigproject / subProj 1022' project: general settings of 'bt 1022 0' build configuration were updated

--- .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
+++ .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
@@ -6,6 +6,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 object subProj_bt_1022_0 : BuildType({
     name = "bt 1022 0"
 
+    artifactRules = "+:a.txt => ./"
+
     params {
         password("env.ppwd", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9cd")
     }
