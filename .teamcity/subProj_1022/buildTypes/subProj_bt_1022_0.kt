21275930da3d43323c3ceae51f44a12c56397424
admin <admin@TeamCity>
2021-12-02 12:09:13 +0100

    TeamCity change in 'bigproject / subProj 1022' project: runners of 'bt 1022 0' build configuration were updated

--- .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
+++ .teamcity/subProj_1022/buildTypes/subProj_bt_1022_0.kt
@@ -1,6 +1,7 @@
 package subProj_1022.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object subProj_bt_1022_0 : BuildType({
@@ -17,15 +18,10 @@ object subProj_bt_1022_0 : BuildType({
             enabled = false
             scriptContent = "echo %env.ppwd% > a.txt"
         }
-        step {
-            type = "Bigproject_subProj_1022_Bt10220"
-            executionMode = BuildStep.ExecutionMode.DEFAULT
-            param("sec", "credentialsJSON:03c848ae-0be2-4173-b32d-04c0a8621c8d")
-            param("a", "a")
-            param("b", "b")
-            param("env.ppwd", "credentialsJSON:4f9e06b7-b6b6-44dc-afd1-0cbcb0dd2cde")
-            param("c", "c")
-            param("system.ppwd", "credentialsJSON:4f9e06b7-b6b6-44dc-afd1-0cbcb0dd2cde")
+        powerShell {
+            scriptMode = file {
+                path = "jk.ps1"
+            }
         }
     }
 })
