87e94e382e49f1d7affac43434291891378994c9
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-09-19 15:08:14 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -15,7 +15,26 @@ project {
     val numProjects=12
     val numConfigurationsPerProject=800
 
+for (i in 0..2) {
+        subProject {
+            id("subProjArc_$i")
+            name = "subProjArc $i"
+archived=true
 
+for (j in 0..2) {
+                buildType {
+                    id("subProjArc_bt_$i" + "_$j")
+                    name = "bt Arc $i $j"
+                    
+    vcs {
+        root(AbsoluteId("Adaptiv"))
+    }
+
+                }
+            }
+        
+
+}
     for (i in 0..numProjects) {
         subProject {
             id("subProj_$i")
